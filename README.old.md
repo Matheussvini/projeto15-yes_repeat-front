145197198acf795a266846811ea3ca043c01caba
Matheus Vinicius <matheussvini@outlook.com>
2022-11-23 08:48:21 -0300

    create react app and initial files configs

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# projeto15-yes_repeat-front
+Este é o front-end de um projeto Full Stack em dupla, para treinar habilidades como trabalho em equipe. Um projeto com requisitos abertos, onde devemos criar um e-commerce com funcionalidades de: login; cadastro; listagem de produtos; carrinho e checkout. Decidimos criar o site do - Yes, repeat! - um brechó online no instagram.
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./assets/styles/GlobalStyle";
+import { useState } from "react";
+import UserContext from "./components/Context/context";
+
+function App() {
+
+  const [user, setUser] = useState([]);
+  const [change, setChange] = useState(false);
+
+  const localUserSerializado = localStorage.getItem("localUser");
+  const localUser = JSON.parse(localUserSerializado);
+
+  if (localUser && user.length === 0) {
+    setUser(localUser);
+  }
+
+  return (
+    <UserContext.Provider value={{ user, setUser, change, setChange }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/cadastro" element={<CreateUserPage />} />
+          <Route path="/produtos" element={<ProductsPage />} />
+          <Route path="/adiciona-produto/:category" element={<AddProductsPage />} />
+          <Route path="/sacola" element={<ShoppingBagPage/>} />
+          <Route path="/pagamento" element={<CheckoutPage/>} />
+          
+        </Routes>
+
+        <GlobalStyle />
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
