c46641e30dc0faca35bc9fb29bdc881109800f6d
matheussvini <matheussvini@outlook.com>
2022-11-24 19:33:21 -0300

    feat: login page and add user context to keep logged in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./assets/styles/GlobalStyle";
 import { useState } from "react";
-// import UserContext from "./components/Context/context";
+import UserContext from "./components/Context/context";
 import ProductsPage from "./components/ProductsPage";
 import CreateUserPage from "./pages/CreateUserPage/CreateUserPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   const [user, setUser] = useState([]);
-  // const [change, setChange] = useState(false);
+  const [change, setChange] = useState(false);
 
   const localUserSerializado = localStorage.getItem("localUser");
   const localUser = JSON.parse(localUserSerializado);
@@ -17,11 +18,11 @@ function App() {
   }
 
   return (
-    // <UserContext.Provider value={{ user, setUser, change, setChange }}>
+    <UserContext.Provider value={{ user, setUser, change, setChange }}>
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage/>} /> */}
+        {/* <Route path="/" element={<HomePage />} />*/}
+          <Route path="/login" element={<LoginPage/>} /> 
           <Route path="/cadastro" element={<CreateUserPage />} />
         <Route path="/produtos" element={<ProductsPage />} />
         <Route path="/produto/:id" />
@@ -32,7 +33,7 @@ function App() {
 
       <GlobalStyle />
     </BrowserRouter>
-    // </UserContext.Provider>
+    </UserContext.Provider>
   );
 }
 
